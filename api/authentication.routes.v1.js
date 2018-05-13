6c9c9f3f6694506936d21f10f5f9f864b42d67d2
Reneetjuh21 <r.vangool@live.nl>
2018-05-14 00:38:48 +0200

    register endpoint
    register endpoint init

--- api/authentication.routes.v1.js
+++ api/authentication.routes.v1.js
@@ -39,5 +39,38 @@ router.post('/login', function(req, res) {
 
 });
 
+//
+// Hier gaat de gebruiker registreren.
+// Input: username en wachtwoord
+// ToDo: 
+//	 - zoek de username in de database
+// 	 - achterhalen of de username al in gebruik is, zo niet kan een user aangemaakt worden
+//   - anders is de registratiepoging gefaald - geef foutmelding terug.
+//
+router.post('/register', function(req, res) {
+
+    // Even kijken wat de inhoud is
+    console.dir(req.body);
+
+    // De username en pwd worden meegestuurd in de request body
+    var username = req.body.username;
+    var password = req.body.password;
+
+    // Dit is een dummy-user - die haal je natuurlijk uit de database.
+    var _dummy_username = "test";
+    var _dummy_password = "test";
+
+    // Kijk of de username al in gebruik is genomen
+    if (username == _dummy_username) {
+        // var token = auth.encodeToken(username);
+        // res.status(200).json({
+        //     "token": token,
+        // });
+    } else {
+        res.status(401).json({ "error": "Username already in use, bye" })
+    }
+
+});
+
 // Hiermee maken we onze router zichtbaar voor andere bestanden. 
 module.exports = router;
