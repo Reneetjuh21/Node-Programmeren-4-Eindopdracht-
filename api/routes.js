8a75321a504cbb605cb82281398b636ca0049607
EQQ <epekguze@avans.nl>
2018-05-14 15:04:21 +0200

    /api/studentenhuis
    Started on /api/studentenhuis

--- api/routes.js
+++ api/routes.js
@@ -4,19 +4,28 @@
 var express = require('express');
 var routes = express.Router();
 var db = require('../config/db');
+var auth = require('../auth/authentication.js');
 
 //
-// Geef een lijst van alle actors. Dat kunnen er veel zijn.
+// Maak een nieuw studentenhuis, en geef de gebruikte ID mee.
 //
-routes.get('/actors', function(req, res){
+routes.post('/studentenhuis', function(req, res){
 	res.contentType('application/json');
+	
+	db.query('INSERT INTO Studentenhuis', function(error, rows, fields) {
 
-	db.query('SELECT * FROM actor', function(error, rows, fields) {
-		if (error) { 
+	});
+});
+
+routes.get('/studentenhuis', function(req, res){
+	res.contentType('application/json');
+	
+	db.query('SELECT * FROM Studentenhuis', function(error, rows, fields) {
+		if (error) {
 			res.status(400).json(error);
 		} else {
 			res.status(200).json(rows);
-		};
+		}
 	});
 });
 
