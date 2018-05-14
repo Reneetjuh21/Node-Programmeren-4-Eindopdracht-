34d2522de4d96a551c167da1fea75e0233175160
René van Gool <r.vangool@live.nl>
2018-05-14 16:12:45 +0200

    encryption login and register

--- api/authentication.routes.js
+++ api/authentication.routes.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../config/db');
+var bcrypt = require('bcrypt');
 
 var auth = require('../auth/authentication');
 
@@ -25,21 +26,25 @@ router.post('/login', function(req, res) {
     var password = req.body.password;
 
     //Users ophalen uit de database
-    db.query('SELECT ID, Email, Password FROM user WHERE Email = ?',[ email], function(error, rows, fields) {
+    db.query('SELECT ID, Email, Password FROM user WHERE Email = ?',[email], function(error, rows, fields) {
         if (error) { 
 			res.status(412).json({ "error": "Een of meer properties in de request body ontbreken of zijn foutief" });
 		} else {
-			for (var i = 0; i <= rows.length; i++){
+			for (var i = 0; i < rows.length; i++){
                 var db_email = rows[i].Email;
                 var db_password = rows[i].Password;
 
                 // Kijk of de gegevens matchen. Zo ja, dan token genereren en terugsturen.
-                if (email == db_email && password == db_password) {
-                    var token = auth.encodeToken(email);
-                    res.status(200).json({
-                        "token": token,
-                        "email": email
-                    });
+                if (email == db_email) {
+                    if (bcrypt.compareSync(password, db_password)){
+                        var token = auth.encodeToken(email);
+                        res.status(200).json({
+                            "token": token,
+                            "email": email
+                        });
+                    } else {
+                        res.status(412).json({ "error": "Een of meer properties in de request body ontbreken of zijn foutief" });
+                    }
                 } else {
                     res.status(412).json({ "error": "Een of meer properties in de request body ontbreken of zijn foutief" });
                 }
@@ -67,6 +72,7 @@ router.post('/register', function(req, res) {
     var email = req.body.email;
     var password = req.body.password;
 
+
     if (firstname == '' || lastname == '' || email == '' || password == ''){
         res.status(412).json({ "error": "Een of meer properties in de request body ontbreken of zijn foutief" });
     } else {
@@ -75,17 +81,21 @@ router.post('/register', function(req, res) {
                 res.status(400).json(error);
             } else {
                 if (rows.length == 0){
-                    db.query('INSERT INTO `user`(`Voornaam`, `Achternaam`, `Email`, `Password`) VALUES (?, ?, ?, ?)',[ firstname, lastname, email, password], function(error, rows, fields) {
-                        if(error){
-                            res.status(400).json(error);
-                        } else {
-                            var token = auth.encodeToken(email);
-                            res.status(200).json({
-                                "token": token,
-                                "email": email
-                            });
-                        }
-                    });  
+                    const saltRounds = 10;
+                    bcrypt.hash(password, saltRounds, function(err, hash) {
+                        var encryptedpassword = hash;
+                        db.query('INSERT INTO `user`(`Voornaam`, `Achternaam`, `Email`, `Password`) VALUES (?, ?, ?, ?)',[ firstname, lastname, email, encryptedpassword], function(error, rows, fields) {
+                            if(error){
+                                res.status(400).json(error);
+                            } else {
+                                var token = auth.encodeToken(email);
+                                res.status(200).json({
+                                    "token": token,
+                                    "email": email
+                                });
+                            }
+                        }); 
+                    });
                 } else {
                     for (var i = 0; i < rows.length; i++){
                         var db_email = rows[i].Email;
@@ -93,16 +103,20 @@ router.post('/register', function(req, res) {
                         if (email == db_email) {
                             res.status(401).json({ "error": "De gebruiker die u probeert toe te voegen, gebruikt een emailadres dat al bekend is in onze database. Gebruik een andere." });
                         } else {
-                            db.query('INSERT INTO `user`(`Voornaam`, `Achternaam`, `Email`, `Password`) VALUES (?, ?, ?, ?)',[ firstname, lastname, email, password], function(error, rows, fields) {
-                                if(error){
-                                    res.status(400).json(error);
-                                } else {
-                                    var token = auth.encodeToken(email);
-                                    res.status(200).json({
-                                        "token": token,
-                                        "email": email
-                                    });
-                                }
+                            const saltRounds = 10;
+                            bcrypt.hash(password, saltRounds, function(err, hash) {
+                                var encryptedpassword = hash;
+                                db.query('INSERT INTO `user`(`Voornaam`, `Achternaam`, `Email`, `Password`) VALUES (?, ?, ?, ?)',[ firstname, lastname, email, encryptedpassword], function(error, rows, fields) {
+                                    if(error){
+                                        res.status(400).json(error);
+                                    } else {
+                                        var token = auth.encodeToken(email);
+                                        res.status(200).json({
+                                            "token": token,
+                                            "email": email
+                                        });
+                                    }
+                                }); 
                             });
                         }
                     } 
