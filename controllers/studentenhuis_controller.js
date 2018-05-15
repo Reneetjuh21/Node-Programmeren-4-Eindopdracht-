62c1bd83a0a253e5a9aca89d94b915ffa481e566
EQQ <epekguze@avans.nl>
2018-05-15 18:29:15 +0200

    studentenhuis_controller finished CRUD operations

--- controllers/studentenhuis_controller.js
+++ controllers/studentenhuis_controller.js
@@ -4,34 +4,36 @@ const db = require('../config/db');
 const api_error = require('../models/apierror');
 
 module.exports = {
+
     postNew(req, res, next) {
-        const bufferToken = req.get('Authorization');
-        var subUserToken = bufferToken.substr(7);
         var bufferPayload = '';
-        var decodedUserToken = auth.decodeToken(subUserToken, (err, payload) => {
+        const bufferToken = req.get('Authorization').substr(7);
+
+        var decodedUserToken = auth.decodeToken(bufferToken, (err, payload) => {
             if (err) {
                 const error = new api_error("Niet geautoriseerd (geen valid token)", 401);
-                console.log(err);
                 next(error);
             } else {
                 bufferPayload = payload;
-            
             	 }
             });
-                var userToken = bufferPayload.userID;
-                console.log(userToken);
-        //To-do: check ID of payload, use it to create a new row.
+
+        var userToken = bufferPayload.userID;
+        console.log(userToken);
+
         try {
                 assert(typeof (req.body.naam) === 'string', 'Name must be a string.')
                 assert(typeof (req.body.adres) === 'string', 'Adres must be a string.')
             } catch (ex) {
                 const error = new api_error("Een of meer properties in de request body ontbreken of zijn foutief", 412);
                 next(error);
-                return;
         }
+
         res.contentType('application/json');
+
 	    var insertedNaam = req.body.naam;
-	    var insertedAdres = req.body.adres;
+        var insertedAdres = req.body.adres;
+        
 	    db.query('INSERT INTO studentenhuis (Naam, Adres, UserID) VALUES (?, ?, ?)', [insertedNaam, insertedAdres, userToken], function(error, rows, fields) {
 		if (error) {
 			res.status(400).json(error);
@@ -45,7 +47,9 @@ module.exports = {
     },
 
     getAll(req, res, next) {
+
         res.contentType('application/json');
+
 	    db.query('SELECT * FROM studentenhuis', function(error, rows, fields) {
 		if (error) {
 			res.status(400).json(error);
@@ -56,8 +60,11 @@ module.exports = {
     },
 
     getById(req, res, next) {
+
         var studentenhuisId = req.params.id;
-	    res.contentType('application/json');
+
+        res.contentType('application/json');
+        
 	    db.query('SELECT * FROM studentenhuis WHERE ID = ?', [studentenhuisId], function(error, rows, fields) {
 		if (error) {
 			res.status(400).json(error);
@@ -73,18 +80,21 @@ module.exports = {
     },
 
     putById(req, res, next) {
-        const userToken = req.get('Authorization');
-        var subUserToken = userToken.substr(7);
-        var decodedUserToken = auth.decodeToken(subUserToken, (err, payload) => {
+        var bufferPayload = '';
+        const bufferToken = req.get('Authorization').substr(7);
+
+        var decodedUserToken = auth.decodeToken(bufferToken, (err, payload) => {
             if (err) {
                 const error = new api_error("Niet geautoriseerd (geen valid token)", 401);
                 next(error);
             } else {
-                console.log(payload);
+                bufferPayload = payload;
                 return;
             }
         });
-        //To-do: check ID of payload, if ID was used to create the row, then continue. & add 409 conflict error.
+      
+        var userToken = bufferPayload.userID;
+
         try {
             assert(typeof (req.body.naam) === 'string', 'Name must be a string.')
             assert(typeof (req.body.adres) === 'string', 'Adres must be a string.')
@@ -93,24 +103,32 @@ module.exports = {
             next(error);
             return;
         }
+
         var insertedNaam = req.body.naam;
         var insertedAdres = req.body.adres;
         var insertedId = req.params.id;
+
         res.contentType('application/json');
+
         db.query('SELECT * FROM studentenhuis WHERE ID = ?', [insertedId], function(error, rows, fields) {
             if (error) {
                 res.status(400).json(error);
             } else {
                 if (rows.length !== 0) {
-                    db.query('UPDATE studentenhuis SET naam = ? , Adres = ? WHERE ID = ?', [insertedNaam, insertedAdres, insertedId], function(error, rows, fields) {
-                        if (error) {
-                            res.status(400).json(error);
-                        } else {
-                            db.query('SELECT * FROM studentenhuis WHERE ID = ?', [insertedId], function(error, rows, fields) {
-                                res.status(200).json(rows);
-                            });
-                        }
-                    });
+                    if (rows[0].UserID !== userToken) {
+                        const error = new api_error('Conflict (Gebruiker mag deze data niet wijzigen)', 409);
+                        next(error);
+                    } else {
+                        db.query('UPDATE studentenhuis SET naam = ? , Adres = ? WHERE ID = ?', [insertedNaam, insertedAdres, insertedId], function(error, rows, fields) {
+                            if (error) {
+                                res.status(400).json(error);
+                                } else {
+                                    db.query('SELECT * FROM studentenhuis WHERE ID = ?', [insertedId], function(error, rows, fields) {
+                                        res.status(200).json(rows);
+                                    });
+                                }
+                        }); 
+                    }
                 } else {
                     const error = new api_error("Niet gevonden (huisId bestaat niet)", 404);
                     next(error);
@@ -120,35 +138,43 @@ module.exports = {
     },
 
     deleteById(req, res, next) {
-        const userToken = req.get('Authorization');
-        var subUserToken = userToken.substr(7);
-        var decodedUserToken = auth.decodeToken(subUserToken, (err, payload) => {
+        var bufferPayload = '';
+        const bufferToken = req.get('Authorization').substr(7);
+
+        var decodedUserToken = auth.decodeToken(bufferToken, (err, payload) => {
             if (err) {
                 const error = new api_error("Niet geautoriseerd (geen valid token)", 401);
                 next(error);
             } else {
-                console.log(payload);
+                bufferPayload = payload;
                 return;
             }
         });
-        //To-do: check ID of payload, if ID was used to create the row, then continue.
+        
+        var userToken = bufferPayload.userID;
         var insertedId = req.params.id;
+       
         res.contentType('application/json');
+
         db.query('SELECT * FROM studentenhuis WHERE ID = ?', [insertedId], function(error, rows, fields) {
             if (rows.length !== 0) {
-                db.query('DELETE FROM studentenhuis WHERE ID = ?', [insertedId], function(error, rows, fields) {
-                    if (error) {
-                        res.status(400).json(error);
-                    } else {
-                        const error = new api_error('Studentenhuis is succesvol verwijderd.', 200);
-                        res.status(200).json(error);
-                    }
-                });
+                if (rows[0].UserID !== userToken) {
+                    const error = new api_error('Conflict (Gebruiker mag deze data niet wijzigen)', 409);
+                    next(error);
+                } else {
+                    db.query('DELETE FROM studentenhuis WHERE ID = ?', [insertedId], function(error, rows, fields) {
+                        if (error) {
+                            res.status(400).json(error);
+                        } else {
+                            const error = new api_error('Studentenhuis is succesvol verwijderd.', 200);
+                            res.status(200).json(error);
+                        }
+                    });
+                } 
             } else {
                 const error = new api_error('Niet gevonden (huisId bestaat niet)', 404);
                 next(error);
             };
         });
-        
     }
 }
