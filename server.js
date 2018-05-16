b3c339d0fd76406b847a3fa4ed01623ba71dcbac
Reneetjuh21 <r.vangool@live.nl>
2018-05-16 13:28:07 +0200

    updated db connection

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(expressJWT({
 }));
 
 // configureer de app
-app.set('port', (process.env.PORT | config.webPort));
+app.set('port', (process.env.PORT || config.webPort));
 // app.set('env', (process.env.ENV | 'development'))
 
 // Installeer Morgan als logger
